ea088112b9f9814ac039dfc4bac4d196fb97e36e
srikanthpinisetti <srikanthpinisetti@gmail.com>
2026-04-15 17:22:22 +0000

    New branch dev created from main

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -55,21 +55,20 @@ CREATE OR REPLACE STAGE FROSTBYTE_RAW_STAGE
 
 -- ----------------------------------------------------------------------------
 -- Create the PyPI external access integration
+-- (Skipped: not supported on trial accounts. Use Anaconda channel packages.)
 -- ----------------------------------------------------------------------------
-USE ROLE ACCOUNTADMIN;
+-- USE ROLE ACCOUNTADMIN;
 
--- This is a schema level object
-CREATE OR REPLACE NETWORK RULE DEMO_DB.INTEGRATIONS.PYPI_NETWORK_RULE
-MODE = EGRESS
-TYPE = HOST_PORT
-VALUE_LIST = ('pypi.org', 'pypi.python.org', 'pythonhosted.org', 'files.pythonhosted.org');
+-- CREATE OR REPLACE NETWORK RULE DEMO_DB.INTEGRATIONS.PYPI_NETWORK_RULE
+-- MODE = EGRESS
+-- TYPE = HOST_PORT
+-- VALUE_LIST = ('pypi.org', 'pypi.python.org', 'pythonhosted.org', 'files.pythonhosted.org');
 
--- This is an account level object
-CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION PYPI_ACCESS_INTEGRATION
-ALLOWED_NETWORK_RULES = (PYPI_NETWORK_RULE)
-ENABLED = true;
+-- CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION PYPI_ACCESS_INTEGRATION
+-- ALLOWED_NETWORK_RULES = (PYPI_NETWORK_RULE)
+-- ENABLED = true;
 
-GRANT USAGE ON INTEGRATION PYPI_ACCESS_INTEGRATION TO ROLE DEMO_ROLE;
+-- GRANT USAGE ON INTEGRATION PYPI_ACCESS_INTEGRATION TO ROLE DEMO_ROLE;
 
 
 -- ----------------------------------------------------------------------------
